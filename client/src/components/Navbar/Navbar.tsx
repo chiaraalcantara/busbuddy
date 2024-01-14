614ec26fb533f658ab8ae46c8d5cdd85e834cef6
Edmond <liedmond0527@gmail.com>
2024-01-14 00:12:22 -0500

    added color

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     return (
         // the green and w/o is just temp chnage later
         <header className="w-full fixed top-0 left-0 z-1000 bg-green-300 shadow-xl">
-            <nav className='flex justify-between container items-center gap-x-4 px-4 h-[50px]'>
+            <nav className='flex justify-between container items-center gap-x-4 px-4 h-[70px]'>
                 <h1 className="font-bold text-lg">Magic School Bus xD</h1>
                 <ul className="flex gap-x-8">
                     {links.map((link, index) => (
