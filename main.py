46ab1e09905364fcd77e22b11e133b2e20737ccc
Franklin Ramirez <franklin.ramirez6111@gmail.com>
2024-01-13 19:19:13 -0500

    changed code

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import cv2
 import os
 from os import listdir
 
+import base64, binascii
+
 
 #   TODO Import images from database 
 
@@ -18,32 +20,48 @@ cv2.namedWindow("test")
 # Initialize Image Count 
 img_counter = 0
 
-# Initialize Image List 
-known_face_encodings = []
+# Initialize Image Dictionary 
+known_face_encodings = {}
 
 
 # Loop throuhg all the encoded image ids, then convert it, then store it in the array
-
-
 
 # TODO Helena!
 # Loop through images in the Database. Loop through encoded string. Call the function that turns
 # it back into an image. 
 # Data is called Bus Data c
 # In here we store both the encoded string, and the image id associated to the string. 
-for file in os.listdir(filepath):
-    filename = os.fsdecode(file)
-    if filename.endswith(".jpg"):
-       print(file)
-       image_path = os.path.join(filepath, file)
-       # Convert from b64 to image here!
-       image = face_recognition.load_image_file(image_path)
-       image_encoding = face_recognition.face_encodings(image)[0]
-       known_face_encodings.append(image_encoding) # Store it here, two columns. One for id and one for encoded string  
-       # Extract associated image id.
-       print(len(known_face_encodings))
-    else:
-        continue
+
+# For time being will hard code 3
+# For time being, we hard code bus 29
+
+# Filter first by bus number, then length of that bus number column
+for i in range(3) : # Grab length of column
+    base64_string = "HELENA STRING" #TODO from the database
+    try:
+        image = base64.b64decode(base64_string, validate=True)
+        file_to_save = "name or path of the file to save, let's say, my_image.png"
+        with open(file_to_save, "wb") as f:
+            f.write(image)
+        known_image = face_recognition.load_image_file(image)
+        image_encoding = face_recognition.face_encodings(image)[0]
+        known_face_encodings[image_encoding] = # Name of Child of the same row
+    except binascii.Error as e:
+        print(e)
+        
+# for file in os.listdir(filepath):
+#     filename = os.fsdecode(file)
+#     if filename.endswith(".jpg"):
+#        print(file)
+#        image_path = os.path.join(filepath, file)
+#        # Convert from b64 to image here!
+#        image = face_recognition.load_image_file(image_path)
+#        image_encoding = face_recognition.face_encodings(image)[0]
+#        known_face_encodings.append(image_encoding) # Store it here, two columns. One for id and one for encoded string  
+#        # Extract associated image id.
+#        print(len(known_face_encodings))
+#     else:
+#         continue
 
 stop = 0
 
@@ -86,6 +104,7 @@ while True:
                 # select_imageid 
                 # If true, puts text on CV Window Welcome or Goodbye depending on getting on or off the bus
                 if result[0] : 
+                    face
                     # Check if getting on the bus, if it is the right bus
                     # Check if getting off the bus, if it is the right stop
                     text = "Welcome/Goodbye!" # TODO add the name
